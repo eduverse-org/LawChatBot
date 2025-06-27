a40cbb915469bdc74611c723f75a6429fe9d465f
Mohit Davar <mohitdavar2004@gmail.com>
2025-06-28 04:43:16 +0530

    Small

--- embedder.py
+++ embedder.py
@@ -2,9 +2,10 @@ import yaml
 from langchain_huggingface import HuggingFaceEmbeddings
 
 class Embedder:
-    def __init__(self, config_path="config/config.yaml"):
+    def _init_(self, config_path="config/config.yaml"):
         with open(config_path, "r") as file:
             config = yaml.safe_load(file)
+
         self.embedder = HuggingFaceEmbeddings(
             model_name=config["embedder"]["model_name"],
             model_kwargs={"device": config["embedder"]["device"]}
